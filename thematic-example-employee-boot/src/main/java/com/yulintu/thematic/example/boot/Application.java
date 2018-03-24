188371010f56386bdfc73a4595fc11e3787e538c
HIDE <bowzar@qq.com>
2018-03-24 10:18:44 +0800

    update

--- thematic-example-employee-boot/src/main/java/com/yulintu/thematic/example/boot/Application.java
+++ thematic-example-employee-boot/src/main/java/com/yulintu/thematic/example/boot/Application.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 public class Application {
 
     public static void main(String[] args) {
-        
         GlobalApplicationContext.setApplicationContext(
                 SpringApplication.run(Application.class, args));
     }
