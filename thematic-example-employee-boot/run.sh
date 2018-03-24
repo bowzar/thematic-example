503af7ebc73457a6f9cd390ac9c7bf47b284076c
HIDE <bowzar@qq.com>
2018-03-24 11:18:19 +0800

    update

--- thematic-example-employee-boot/run.sh
+++ thematic-example-employee-boot/run.sh
@@ -1,3 +1,4 @@
 docker -H 192.168.20.10 stop thematic-example-employee-boot
 docker -H 192.168.20.10 rm thematic-example-employee-boot
+
 docker -H 192.168.20.10 run --name thematic-example-employee-boot -d -p 6030:8080 -e PARAMS="--spring.profiles.active=dev" -v /hide/Develop/Docker/thematic-example-employee/config:/config -v /hide/Develop/Docker/thematic-example-employee/logs:/build 192.168.20.10:8082/thematic-example-employee-boot
