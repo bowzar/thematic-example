c4848f8105ac24eed9f0ced9a98398db4a3abf40
HIDE <bowzar@qq.com>
2018-04-04 15:37:50 +0800

    update

--- script/jenkins_build.sh
+++ script/jenkins_build.sh
@@ -7,6 +7,7 @@ docker -H 192.168.20.10 ps -a | grep "thematic-example-employee-boot" | awk "{pr
 docker -H 192.168.20.10 run --name thematic-example-employee-boot -d -p 6030:8080 -e PARAMS="--spring.profiles.active=pro" -v /hide/Develop/Docker/thematic-example-employee/config:/config -v /hide/Develop/Docker/thematic-example-employee/logs:/build 192.168.20.10:8082/thematic-example-employee-boot
 
 docker -H 192.168.20.10 images | grep "hours ago" | grep "thematic-example-employee-boot" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
+docker -H 192.168.20.10 images | grep "days ago" | grep "thematic-example-employee-boot" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
 docker -H 192.168.20.10 images | grep "weeks ago" | grep "thematic-example-employee-boot" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
 docker -H 192.168.20.10 images | grep "months ago" | grep "thematic-example-employee-boot" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
 docker -H 192.168.20.10 images | grep "years ago" | grep "thematic-example-employee-boot" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
@@ -23,6 +24,7 @@ docker -H 192.168.20.10 ps -a | grep "thematic-example-employee-web" | awk "{pri
 docker -H 192.168.20.10 run --name thematic-example-employee-web -d -p 6031:8081  192.168.20.10:8082/thematic-example-employee-web
 
 docker -H 192.168.20.10 images | grep "hours ago" | grep "thematic-example-employee-web" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
+docker -H 192.168.20.10 images | grep "days ago" | grep "thematic-example-employee-boot" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
 docker -H 192.168.20.10 images | grep "weeks ago" | grep "thematic-example-employee-web" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
 docker -H 192.168.20.10 images | grep "months ago" | grep "thematic-example-employee-web" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
 docker -H 192.168.20.10 images | grep "years ago" | grep "thematic-example-employee-web" | awk "{print \$3}" | xargs --no-run-if-empty docker -H 192.168.20.10 rmi
