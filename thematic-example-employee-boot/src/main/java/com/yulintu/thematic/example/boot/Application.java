afd86ba3219678f7f8c781884a32cc37af7fa1e7
HIDE <bowzar@qq.com>
2018-04-12 22:17:06 +0800

    update

--- thematic-example-employee-boot/src/main/java/com/yulintu/thematic/example/boot/Application.java
+++ thematic-example-employee-boot/src/main/java/com/yulintu/thematic/example/boot/Application.java
@@ -6,10 +6,59 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
+
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+      return   RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultCacheConfig())
+                .initialCacheNames(cacheNames())
+                .withInitialCacheConfigurations(cacheConfigurations())
+        .transactionAware()
+        .build();
+
+
+
+    }
+
+    private RedisCacheConfiguration defaultCacheConfig(){
+       return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(10))
+                .disableCachingNullValues();
+    }
+
+    private HashMap<String,RedisCacheConfiguration> cacheConfigurations(){
+        HashMap<String, RedisCacheConfiguration> map = new HashMap<>();
+        map.put("default",defaultCacheConfig());
+        map.put("employee",RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofSeconds(20))
+        .disableCachingNullValues()
+
+        );
+
+        return  map;
+    }
+
+    private  HashSet<String> cacheNames(){
+        HashSet<String> names = new HashSet<>();
+        names.add("default");
+        names.add("employee");
+
+        return  names;
+    }
+
 
     public static void main(String[] args) {
         GlobalApplicationContext.setApplicationContext(
